2f1f26f28ac14588d784d0965e5f69853bd0b6dc
islom <iabdugofurov99@gmail.com>
2024-09-12 18:13:51 +0500

    shox uchun push

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,7 +1,3 @@
-// export { default as AboutPage } from './AboutPage';
-// export { default as DrivePage } from './DrivePage';
-// export { default as HomePage } from './HomePage';
-// export { default as ModelsIDPage } from './ModelsIDPage';
 export { default as HomePage } from './HomePage';
 export { default as NewsPage } from './NewsPage';
 export { default as ContactPage } from './ContactPage';
